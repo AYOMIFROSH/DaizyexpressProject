5a0fac3b660cd684eadd276edc21db89a1277e8a
declinboi <jacobclinton2234@gmail.com>
2024-11-10 20:52:04 +0100

    now

--- Frontend/src/Components/Navbar.tsx
+++ Frontend/src/Components/Navbar.tsx
@@ -11,40 +11,47 @@ export const MenuLink = [
   },
   {
     paths: "/",
-    link: "Services",
+    link: "Settings",
   },
   {
     paths: "/",
-    link: "Contact",
+    link: "Exchange",
+  },
+  {
+    paths: "/",
+    link: "One legal",
+  },
+  {
+    paths: "/",
+    link: "Wallet",
   },
 ];
 
 const Navbar = () => {
-
-    const [showMenu, setShowMenu] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
 
   const toggleMenu = () => {
     setShowMenu(!showMenu);
   };
   return (
-    <div className="bg-yellow-300 shadow-md">
+    <div className="bg-gradient-to-r from-yellow-300 to-yellow-700 shadow-md">
       <div className="container  ">
-        <div className=" flex items-center justify-between gap-4">
+        <div className=" flex  items-center justify-between gap-16">
           <a
             href="#"
-            className="font-bold flex text-3xl sm:text-4xl gap-1 py-3 "
+            className="font-bold flex text-2xl  md:text-3xl lg:text-4xl gap-1 py-3  "
           >
             <img src={Logo} alt="" className="w-20 rounded-full " />
             Server Manager
           </a>
-          <div className="md:block hidden">
+          <div className="lg:flex hidden">
             <ul className="flex items-center gap-8">
               {MenuLink.map(({ paths, link }) => {
                 return (
-                  <li key={paths} className="cursor-pointer py-4">
+                  <li key={paths} className="cursor-pointer  py-4">
                     <NavLink
                       to={paths}
-                      className="text-lg font-medium hover:text-blue-500 py-2 hover:border-b-2 hover:border-blue-500 transition-all duration-500"
+                      className="text-lg font-medium hover:text-blue-500 py-2  hover:border-b-2 hover:border-blue-500 transition-all duration-500"
                     >
                       {link}
                     </NavLink>
@@ -61,7 +68,6 @@ const Navbar = () => {
           </div>
           {/* mobile view */}
           <div className="md:hidden flex items-center gap-4">
-        
             {showMenu ? (
               <HiMenuAlt1
                 onClick={toggleMenu}
