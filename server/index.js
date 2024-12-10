6caae7cdeceb98615125fa1d01821d324ada7449
Temitayo <rhymeanabel@gmail.com>
2024-12-10 20:05:27 +0100

    server

--- server/index.js
+++ server/index.js
@@ -10,15 +10,14 @@ require('dotenv').config();
 const dbAltHost = process.env.DB_ALT_HOST;
 
 // MIDDLEWARES
+    // origin: 'http://localhost:5173', 
 
 // Define CORS options
 const corsOptions = {
-    origin: ['https://daizyexpress.vercel.app'], 
-    // origin: 'http://localhost:5173', 
+    origin: 'https://daizyexpress.vercel.app/', 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
     allowedHeaders: ['Content-Type', 'Authorization'], 
     credentials: true,
-    optionsSuccessStatus: 200
 };
 
 // Apply CORS middleware with options
