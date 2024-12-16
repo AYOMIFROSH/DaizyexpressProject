afb15a1fe10c0c2a6bcbbb2e96cbf5fb887fcf85
Faruqoloyede <oloyedefaruq2@gmail.com>
2024-12-16 03:08:24 +0100

    refactor code

--- Frontend/src/Components/Top.tsx
+++ Frontend/src/Components/Top.tsx
@@ -1,33 +1,4 @@
-// import { useState } from "react";
-// import logo from "../assets/logo.png"
-// import { Image, Typography } from "antd";
 
-// import { useAuth } from "../Context/useContext";
-// import { FaBars } from "react-icons/fa6";
-// import MobileNav from "./MobileNav";
-
-
-//  const Top = ({isAdmin} : {isAdmin: boolean}) => {
-//   const {  userData } = useAuth();
-//   const [isNavbarOpen, setIsNavbarOpen] = useState<boolean>(false);
-
-//   // const toggleNavbar = (prev:boolean)=> setIsNavbarOpen(!prev);
-
-  
-//    return (
-//     <header className="h-[100px] flex items-center justify-between fixed top-0 z-40 w-full bg-white px-6 py-4 nav">
-//         <Image src={logo} alt="logo" width={200} preview={false} style={{cursor: "pointer"}}></Image>
-//           <div className="flex gap-3 ">
-//             {/* <FaRegUserCircle className="text-4xl" /> */}
-//               <Typography.Paragraph className=" text-[16px] hidden sm:block">{isAdmin ? `Welcome! ${userData.userName}` : `welcome! ${userData.userName}`}</Typography.Paragraph>
-//             </div>
-//             <FaBars className="w-5 h-5 block md:hidden cursor-pointer" onClick={()=> setIsNavbarOpen((prev)=> !prev)}/>
-//               {isNavbarOpen ? <MobileNav isAdmin={false} />  : null}
-//     </header>
-//    )
-//  }
- 
-//  export default Top
 
 import { Link } from "react-router-dom";
 import { FaRegUserCircle } from "react-icons/fa";
@@ -89,7 +60,7 @@ type SideLink = {
           <div className="flex gap-3 max-sm:hidden ">
             <FaRegUserCircle className="text-4xl max-sm:text-2xl" />
               <Typography.Paragraph className=" text-[20px] max-sm:text-[16px]">{isAdmin ? `welcome${userData.userName}` : `welcome${userData.userName}`}</Typography.Paragraph>
-            <span className="text-[20px] max-sm:text-[16px] font-medium text-yellow-400" onClick={handleLogout}>Logout</span>
+            <span className="text-[20px] max-sm:text-[16px] font-medium text-yellow-400 cursor-pointer" onClick={handleLogout}>Logout</span>
             </div>
            
             <div className="hidden max-sm:flex flex-1 items-center justify-end">
