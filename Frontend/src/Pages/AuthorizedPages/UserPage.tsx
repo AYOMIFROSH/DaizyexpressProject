118b029fcb95bc4400206e12b1032d2351e488a8
Nabothdaniel <nabothdaniel44@gmail.com>
2025-01-23 14:51:38 +0100

    patch

--- Frontend/src/Pages/AuthorizedPages/UserPage.tsx
+++ Frontend/src/Pages/AuthorizedPages/UserPage.tsx
@@ -1,12 +1,8 @@
-import { Divider } from "antd";
 import Analytic from "../../Components/DashboardUser/Analytic";
 import Sidebar from "../../Components/Sidebar";
 import Top from "../../Components/Top";
-import { useAuth } from "../../Context/useContext";
-import PaymentForm from "./PaymentForm";
 
 const UserPage = () => {
-  const { isPayed } = useAuth();
   return (
     <>
       {
