2620c4d913a85d6c08c57592bd4e3fdaa4418e36
Temitayo <rhymeanabel@gmail.com>
2025-01-29 17:45:56 +0100

    server

--- server/index.js
+++ server/index.js
@@ -48,16 +48,10 @@ app.use('/api/files', fileRouter);
 app.use('/api/admin', adminRoutes);
 app.use('/api/payment', paymentRoute); 
 
-// const BASE_URL =
-//     process.env.NODE_ENV === 'production'
-//         ? process.env.BASE_URL_PRODUCTION || 'https://daizyexserver.vercel.app'
-//         : process.env.BASE_URL_DEVELOPMENT || 'http://localhost:3000';
-
-// app.locals.BASE_URL = BASE_URL;
-app.use('/assets', express.static('assets'));
-
 app.set('views', path.join(__dirname, 'views'));  
 app.set('view engine', 'ejs'); 
+
+app.use(express.static(path.join(__dirname, 'server')));
 
 
 // General Global Error Handler
